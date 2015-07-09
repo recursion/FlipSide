d8d356581a1d7f51fc28f68d731ab6c802c2da72
Michael Symmes <msymmes@gmail.com>
2015-07-08 20:11:10 -0400

    Wired the config module up

---
+++ app/config.example.js
@@ -0,0 +1,9 @@
+// Use this as a template for starting you local/dev config.js settings
+// make sure to always have config.js in your .gitignore so that your 
+// private data is not sent to github
+// create your api key at https://public.sandbox.exchange.coinbase.com/settings
+module.exports = {
+  passphrase: '<PUTYOURPASSHERE>',
+  key: '<somesuperlongkey>',
+  b64secret: '<crazybighashkeygoeshere>'
+};
